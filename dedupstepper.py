e8d62f7815bb02080163ea08fc3d9fdcd5ab685e
Oscar Koeroo <okoeroo@gmail.com>
2021-09-04 22:52:47 +0200

    Multi processing pool reading files in workers

--- dedupstepper.py
+++ dedupstepper.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
+import asyncio
+import aiosqlite
 import sys
 import os
 import argparse
 import hashlib
+
+import aiofiles
+import asyncio
+import json
 
 def check_correctness(parser, args):
     if args.path is None:
@@ -27,11 +33,24 @@ def argparsing(exec_file):
                         default=None,
                         type=str)
 
+    parser.add_argument("--procnum",
+                        dest='procnum',
+                        help="Number of processes.",
+                        default=None,
+                        type=int)
+
     args = parser.parse_args()
     if not check_correctness(parser, args):
         sys.exit(1)
 
     return args
+
+
+async def ahash(filepath):
+    async with aiofiles.open(filepath, mode='rb') as f:
+        b = await f.read()
+        readable_hash = hashlib.sha256(b).hexdigest()
+        return readable_hash
 
 
 class FileObj:
@@ -41,9 +60,16 @@ class FileObj:
 
         self.filepath = filepath
         self.obj = {}
+        self.obj['filepath'] = self.filepath
 
-        self.update()
-        self.hash()
+        # self.update()
+        # self.hash()
+
+
+    async def ahash(self):
+        h = await ahash(self.filepath)
+        print(h)
+        self.obj['hash'] = h
 
 
     def hash(self):
@@ -52,14 +78,26 @@ class FileObj:
 
         with open(self.filepath, "rb") as f:
             bytes = f.read() # read entire file as bytes
-            readable_hash = hashlib.sha256(bytes).hexdigest();
+            readable_hash = hashlib.sha256(bytes).hexdigest()
 
             self.obj['hash'] = readable_hash
-            return readable_hash
+
+
+    def chash(self):
+        if not os.path.isfile(self.filepath):
+            return
+
+        h_s = hashlib.sha256()
+
+        with open(self.filepath, 'rb') as f:
+            # 16 * 1024 * 64 = 1M
+            while chunk := f.read(16 * 1024 * h_s.block_size):
+                h_s.update(chunk)
+
+        self.obj['chash'] = h_s.hexdigest()
 
 
     def update(self):
-        self.obj['filepath'] = self.filepath
         self.obj['statinfo'] = os.stat(self.filepath)
 
         self.obj['isfile']   = os.path.isfile(self.filepath)
@@ -72,13 +110,35 @@ class FileObj:
         print(self.obj)
 
 
-### MAIN
-if __name__ == "__main__":
-    args = argparsing(os.path.basename(__file__))
+async def test_example(loop):
+    conn = await aiosqlite.connect('sqlite.db', loop=loop)
+    cur = await conn.cursor()
+    await cur.execute("SELECT 42;")
+    r = await cur.fetchall()
+    print(r)
+    await cur.close()
+    await conn.close()
 
+
+
+
+def doit(fo):
+    fo.update()
+    fo.hash()
+    fo.chash()
+    #fo.print()
+
+    return fo
+
+
+from multiprocessing import Pool
+
+
+async def main(args):
     mem = []
 
-    print(args.path)
+    print("... ->", args.path)
+
     for root, dirs, files in os.walk(args.path, topdown=False):
         for name in files:
             fo = FileObj(os.path.join(root, name))
@@ -88,5 +148,24 @@ if __name__ == "__main__":
             fo = FileObj(os.path.join(root, name))
             mem.append(fo)
 
-    for m in mem:
-        m.print()
+    # Deploy action in pool
+    with Pool(args.procnum) as p:
+        res = p.map(doit, mem)
+
+    for r in res:
+        r.print()
+
+
+### MAIN
+if __name__ == '__main__':
+    args = argparsing(os.path.basename(__file__))
+
+    # asyncio.run(aread())
+
+    asyncio.run(main(args))
+
+#    loop = asyncio.new_event_loop()
+#    asyncio.set_event_loop(loop)
+#    loop = asyncio.get_event_loop()
+#    loop.run_until_complete(test_example(loop))
+
